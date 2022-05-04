e32e418209b8396542a9b626799cad1d3344a689
EC2 Default User <ec2-user@ip-172-31-1-81.ec2.internal>
2022-05-04 20:57:34 +0000

    rails7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersCont
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
@@ -6,5 +6,17 @@ class UsersCont
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy
+  resources :post_images, only: [:new, :create, :index, :show, :destroy,
   ]
 
   get "homes/about" => "homes#about"
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update
+  ]
 end
